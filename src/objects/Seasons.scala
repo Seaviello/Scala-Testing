35199662089819fccaa091f2a14e5586b52b9fe0
Seavus <t.kawik@o2.pl>
2016-02-21 17:32:50 +0100

    Initial commit

---
+++ src/objects/Seasons.scala
@@ -0,0 +1,14 @@
+package objects
+
+import objects.Year.Month
+
+/**
+  * Author: Tomasz Kawik
+  * Date: 21.02.2016
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    val april: Month = Year.get(3).get
+    println(april.fullName + " is in " + Year.getSeason(april).get)
+  }
+}
